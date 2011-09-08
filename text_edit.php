e9a81999519bf2457290f97dfbbfe5938394de7d
unknown <Sofie@.(none)>
2011-09-08 12:05:34 +0200

    Dette er en besked

--- text_edit.php
+++ text_edit.php
@@ -2,6 +2,7 @@
 
 <html>
 <head>
+	<!-- This is a comment //-->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Text editor</title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
