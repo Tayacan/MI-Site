e5c24f0301ae2d8c56abca735effdd08438cc9a6
Maya Saietz <mayasaietz@yahoo.dk>
2011-09-08 16:35:00 +0200

    Changed doctypes to xhtml rather than html5.

--- text_edit.php
+++ text_edit.php
@@ -1,10 +1,10 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<!-- This is a larger comment :O //-->
 	<!-- Comment fra maya! -->
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Text editor</title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
 	
