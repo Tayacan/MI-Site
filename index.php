b00a119d0375270012c43d2464d86625090039da
Maya Saietz <mayasaietz@yahoo.dk>
2011-09-08 13:21:48 +0200

    Added index file - you should now be able to go directly to localhost/directory rather than localhost/directory/text_edit.php.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+</head>
+<body>
+
+<?php
+	echo "Hej, jeg er skrevet med php.<br />";
+	echo "<a href='text_edit.php'>Text editor</a><br />";
+?>
+
+</body>
+</html>
