e96c64850464db4142aef8ca20b08f77c5489da4
root <root@GLaDOS.(none)>
2011-11-02 09:31:05 +0100

    Playing with som css... The blue color is sort of random.

---
+++ layout_test/index.php
@@ -0,0 +1,27 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title></title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+	<a href="#">
+	<div class="box">
+		<h3><span>Header</span></h3>
+		<p>
+			This is text inside a box. Trying with more text. Does it wrap proberbly? Yes it does!
+		</p>
+	</div>
+	</a>
+	<a href="#">
+	<div class="box">
+		<h3><span>Header2</span></h3>
+		<p>
+			Second box, with more text. How does it look?
+		</p>
+	</div>
+	</a>
+</body>
+
+</html>
