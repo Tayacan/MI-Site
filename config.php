7a550c95dc257247d084dc9c56ccf745222353b3
Maya Saietz <mayasaietz@gmail.com>
2011-11-02 09:55:52 +0100

    Made a config file for the database. Nothing in it except a comment.

---
+++ config.php
@@ -0,0 +1,10 @@
+<!--
+        This file is for configuring the database. 
+        Every time you make changes to the structure
+        of the database, you'll need to update this 
+        file to do the same automatically. Remember
+        to make if statements to check if the 
+        table/column/whatever you're changing isn't
+        already configured as it should be.
+-->
+
