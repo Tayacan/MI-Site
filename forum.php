766e4935c06cb2b5224b1490042efc3c9df7780a
unknown <Simon@.(none)>
2011-11-03 10:59:52 +0100

    added forum.php

---
+++ forum.php
@@ -0,0 +1,9 @@
+<?php //this is the php datebase code
+$forbindelse=mysql_connect("localhost","root","");
+mysql_selct_db("miForum");
+?>
+<?php require("util.php");//thus is the html top
+	top();
+?>
+<?php bottom();//this is the html bottom
+?>
