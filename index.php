50837ae6ed752dc0bb3f01fa71ac310496e5db80
Maya Saietz <mayasaietz@gmail.com>
2011-11-20 12:57:09 +0100

    Arbejde på den nye forside

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 	top();
 ?>
 
-<div>
+<div style="margin-right: 15px; margin-bottom: 20px;">
 
 <?php
-	echo "Index file <br />";
-	require('file_viewer.php'); // See all files in current directory.
+//	echo "Index file <br />";
+//	require('file_viewer.php'); // See all files in current directory.
 ?>
+
+	<div class="box" style="float: left; width: 100%; height: 200px;">
+		<h3><span>Nyheder</span></h3>
+		<div style="float: left;margin-left: 5px; width: 50%; height: 150px; background-color: #FFFFFF" > </div>
+		<div style="margin-left: 5px; width: 50%; height: 150px; background-color: #FFFFFF" > </div>
+	</div>
+
 </div>
 <?php
 	bottom();
