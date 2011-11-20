75a34ab8c81df70b499870fbcd0c85d92d4e21a1
Simon Ringive <reklamefri@hotmail.com>
2011-11-20 15:15:53 +0100

    Klar til merge

--- forum.php
+++ forum.php
@@ -2,8 +2,17 @@
 $forbindelse=mysql_connect("localhost","root","");
 mysql_select_db("miForum");
 ?>
-<?php require("util.php");//thus is the html top
+<?php require_once("util.php");//thus is the html top
 	top();
 ?>
+<h1> Admin menu </h1>
+<p>
+	<a href="opretcat.php">Opret kategori<a/><br/>
+	<a href="rediger.php">Rediger i kategori<a/><br/>
+	
+</p>
+<div class='kategori'>
+
+</div>
 <?php bottom();//this is the html bottom
 ?>
