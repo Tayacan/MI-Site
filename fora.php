4409503964e57690d9fe9be8323e2cbe147cc653
Jonas Kvist <jonas@.(none)>
2011-11-20 17:40:35 +0100

    Klar til merge

---
+++ fora.php
@@ -0,0 +1,13 @@
+<?php require("util.php");//thus is the html top
+ top();
+?>
+<div class='fora'>
+
+<a href="opretforum.php">Opret fora<a/><br/>
+
+<a href="redigerforum.php">Rediger fora<a/><br/>
+
+</div>
+
+<?php bottom();//this is the html bottom
+?>
