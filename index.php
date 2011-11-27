6d6e11db06ca91c1260acf964491652f669f48ef
Maya Saietz <mayasaietz@gmail.com>
2011-11-27 01:50:19 +0100

    Making pretty boxes for the front page

--- index.php
+++ index.php
@@ -3,8 +3,21 @@
 	top();
 ?>
 
-<div>
-Ting
+<div style="">
+
+	<div class="box" style="width: 625px; height: 250px;">
+		<h3>Seneste forum-aktivitet</h3>
+		<div style="width:55%;height:100%;float:left;">
+			hej1
+		</div>
+		<div style="height:100%;float:left;">
+			hej2
+		</div>
+	</div>
+	<div class="box" style="width: 625px; height: 250px;">
+		<h3>Seneste artikler</h3>
+	</div>
+
 </div>
 <?php
 	bottom();
