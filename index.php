63952ae4667b36695b421233f2832f091e895de9
Maya Saietz <mayasaietz@gmail.com>
2011-11-27 02:28:41 +0100

    The front page now shows the latest article and 200 characters of the text.

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
-	require('util.php');
+	require_once('util.php');
+	require_once('connect.php');
 	top();
+
+	/*
+	 * Code for grabbing the latest articles
+	 */
+	$query = "SELECT * FROM articles ORDER BY articleID DESC";
+	$articles = mysql_query($query) or die(mysql_error());
+
+	// Get the latest written article - the first result.
+	$latestArticle = mysql_fetch_array($articles);
+
+	// Get the next 3 articles for display
+	$arts = array();
+	$arts[0] = mysql_fetch_array($articles);
+	$arts[1] = mysql_fetch_array($articles);
+	$arts[2] = mysql_fetch_array($articles);
 ?>
 
 <div style="">
@@ -16,6 +32,25 @@
 	</div>
 	<div class="box" style="width: 625px; height: 250px;">
 		<h3>Seneste artikler</h3>
+		<a href="viewArticle.php?id=<?php echo $latestArticle['articleID']; ?>" style="text-decoration: none; color: black;">
+		<div style="width:55%;height:80%;float:left;text-align:justify;">
+			<?php
+				if($latestArticle !== null) {
+					echo "<h2>".$latestArticle['title']."</h2>";
+					$stringToPrint = $latestArticle['text'];
+					$stringToPrint = substr($stringToPrint, 0, 200)."...";
+
+					echo '<script type="text/javascript" src="google-code-prettify/src/prettify.js"></script>';
+					echo '<script type="text/javascript" src="text_edit.js"></script>';
+					echo '<script type="text/javascript">';
+					//echo 'window.onload = function () {prettyPrint(); };';
+					echo 'var textToWrite = "'.mysql_real_escape_string($stringToPrint).'";';
+					echo 'textToWrite = view_text(textToWrite);';
+					echo 'document.write(textToWrite);';
+					echo '</script>';
+				}
+			?>
+		</div></a>
 	</div>
 
 </div>
