1987b9dfd413b8724fff6231c5571a627c972ec7
Maya Saietz <mayasaietz@gmail.com>
2011-12-01 10:56:57 +0100

    Limit on text fields when registering

--- user_admin/register.php
+++ user_admin/register.php
@@ -14,17 +14,17 @@
 ?>
 	<form method="post" action="registerBackend.php">
 		Desired username: <br />
-		<input type="text" name="username" /><br /><br />
+		<input maxlength="15" type="text" name="username" /><br /><br />
 		Password: <br />
 		<input type="password" name="password1" /><br /><br />
 		Retype password: <br />
 		<input type="password" name="password2" /><br /><br />
 		First name: <br />
-		<input type="text" name="firstname" /><br /><br />
+		<input maxlength="30" type="text" name="firstname" /><br /><br />
 		Last name: <br />
-		<input type="text" name="lastname" /><br /><br />
+		<input maxlength="30" type="text" name="lastname" /><br /><br />
 		E-mail: <br />
-		<input type="text" name="email" /><br /><br />
+		<input maxlength="30" type="text" name="email" /><br /><br />
 		<input type="submit" value"Registrer" name="register" />
 	</form>
 <?php
