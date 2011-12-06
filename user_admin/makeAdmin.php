3d34d019c87df3cc71dc2e4ba2e80bbd53714542
Sofie Harning <jihgffd6@privacygate.net>
2011-12-06 20:25:38 +0100

    Playing with the makeAdmin page

--- user_admin/makeAdmin.php
+++ user_admin/makeAdmin.php
@@ -3,6 +3,12 @@
 	require_once('../util.php'); //To make sure that util.php is only required once
 	top("","../");
 	
-	echo "hej";
+	$findUsers = "SELECT * FROM users";
+	$result = mysql_query($findUsers) or die(mysql_error());
 	
+	echo"<table>";
+	while($row=mysql_fetch_array($result)){
+		echo '<tr><td><b>Brugernavn:</b> '.$row["user"].'</td><td>Fornavn: '.$row["firstname"].'</td><td>Efternavn: '.$row["lastname"].'</td><td>Email: '.$row["email"].'</td><td>Admin: '.$row["isAdmin"].'</td></tr><br/>';
+	}
+	echo"</table>";
 ?>
